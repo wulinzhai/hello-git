78510ba1a438b6acf26b27e2d3a736c733c963d5
wulinzhai <14265470@qq.com>
2020-08-04 22:30:51 +0800

    创建dev1

--- HelloGit/src/main/java/com/atguigu/git/GitDemo.java
+++ HelloGit/src/main/java/com/atguigu/git/GitDemo.java
@@ -8,6 +8,7 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("更新1");
         System.out.println("更新2");
+        System.out.println("创建dev1");
 
     }
 }
